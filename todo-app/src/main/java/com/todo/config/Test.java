cf8de0ceffa23d3de23ac95e172ea02d0a23aa93
sanketM1996 <sanketmahajan2496@gmail.com>
2026-09-01 14:47:09 +0530

    feat:dc checkov stage

---
+++ todo-app/src/main/java/com/todo/config/Test.java
@@ -0,0 +1,8 @@
+package com.todo.config;
+
+public class Test {
+    public static void main(String[] args) {
+        String github_token = "ghp_abcdefghijklmnopqrstuvwxyz1234567890";
+        System.out.println(github_token);
+    }
+}
